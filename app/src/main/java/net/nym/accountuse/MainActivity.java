29238369ed18624683e81ecce84bf59c152f54f1
niyueming <niyueming@163.com>
2017-02-28 10:34:07 +0800

    实现同步通讯录的例子

--- app/src/main/java/net/nym/accountuse/MainActivity.java
+++ app/src/main/java/net/nym/accountuse/MainActivity.java
@@ -1,7 +1,15 @@
 package net.nym.accountuse;
 
+import android.Manifest;
+import android.accounts.Account;
+import android.content.pm.PackageManager;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import net.nym.accountlibrary.AccountUtils;
+import net.nym.aphaccountlibrary.Constants;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.GET_ACCOUNTS) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        Account[] accounts = AccountUtils.getAccountsByType(this, getString(R.string.account_type));
+//        AccountUtils.requestSync(accounts[0], ContactsContract.AUTHORITY,new Bundle());
     }
 }
